6b72f4e9086f073f36655d56a2c5b87788214c6a
Susan Nio <susan.wen88@gmail.com>
2018-08-03 11:59:22 +0700

    update the user seeder

--- finalProject/seeders/20180803042459-add_initial_user.js
+++ finalProject/seeders/20180803042459-add_initial_user.js
@@ -4,22 +4,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     
       return queryInterface.bulkInsert('Users', [{
-        name: 'susan',
-        password: 'd29e674b947a0f80779113cb5cb93069',
-        salt: '35b9722d098aecfa8a87ad7495e1ddbf',
+        username: 'susan',
+        password: '1909d529f01e579ab6e5c518ba50747b',
+        salt: 'e07699cad3d27e12c0a86ec4fd57c171',
         email: 'susan@mail.com',
         createdAt: new Date,
         updatedAt: new Date
       },
       {
-        name: 'susi',
+        username: 'susi',
         password: '52865065be2dc111d3e903df21da364b',
         salt: '56bb2ad13110b3efde6f82bc70ed62be',
         email: 'susi@mail.com',
         createdAt: new Date,
         updatedAt: new Date
       },{
-        name: 'windra',
+        username: 'windra',
         password: '8a6c0915602264fac6d98ca2fe3788e9',
         salt: 'a29328f0f456b28b028434c27acd6fbf',
         email: 'windra@mail.com',
